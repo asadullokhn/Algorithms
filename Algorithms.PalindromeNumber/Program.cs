81d32fcbe2a347a2e53d0da1a46876565475097d
asadullokhn <87550552+asadullokhn@users.noreply.github.com>
2022-05-04 10:24:21 +0500

    Palindrome

---
+++ Algorithms.PalindromeNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿static bool IsPalindrome(int x)
+{
+    string sx = x.ToString();
+
+    for (int i = 0; i < sx.Length / 2; i++)
+        if (sx[i] != sx[sx.Length - i - 1])
+            return false;
+
+    return true;
+}
+
+Console.WriteLine(IsPalindrome(121));
