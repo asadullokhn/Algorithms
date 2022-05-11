cd277d945780bbee4a81a8d97547bb7183160066
Asadullokh Nurullaev <87550552+asadullokhn@users.noreply.github.com>
2022-05-11 13:08:01 +0500

    WeekDays

---
+++ Algorithms.WeekDays/Program.cs
@@ -0,0 +1,4 @@
+﻿int[] inputs = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+var date = new DateTime(inputs[2], inputs[1], inputs[0]);
+Console.WriteLine(date.DayOfWeek.ToString().ToUpper());
